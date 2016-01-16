4f827f2a8197cdc1d8f5745dc8f95d2e164a463f
KristianTingler <Kristian.Tingler@outlook.com>
2016-01-16 22:24:45 +0100

    NiceButton added

---
+++ NiceButtonInWPF/NiceButtonInWPF/MainWindow.xaml.vb
@@ -0,0 +1,11 @@
+﻿Class MainWindow
+
+    Private Sub NiceButton_MouseLeftButtonUp(sender As Object, e As MouseButtonEventArgs)
+        If ((sender IsNot Nothing) AndAlso (TypeOf sender Is NiceButton)) Then
+            Dim nb As NiceButton = CType(sender, NiceButton)
+
+            nb.IsSelected = Not nb.IsSelected
+        End If
+    End Sub
+
+End Class
